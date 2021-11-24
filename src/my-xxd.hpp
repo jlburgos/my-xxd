fe04d00654991cbd23078645bc79d884df7e4051
Juan Burgos <juanlb1988@gmail.com>
2021-11-24 10:41:48 -0500

    Migrating tool written for a different project to its own git repo

---
+++ src/my-xxd.hpp
@@ -0,0 +1,32 @@
+#ifndef __MY_XXD_HPP
+#define __MY_XXD_HPP
+
+/**
+ * @file my-xxd.hpp
+ * @author Juan Burgos
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+
+struct out_name {
+    std::string name;
+    std::string ext;
+};
+
+struct out_name get_out_name(const std::string src);
+std::vector<unsigned int> convert_in_file(const std::string src);
+bool write_out_file(const std::vector<unsigned int> &values, const struct out_name labels, const std::string dst);
+
+#endif /* __MY_XXD_HPP */
