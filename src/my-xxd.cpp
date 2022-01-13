c11113c632a047c748530567f98de2f591d5e1fa
Juan Burgos <juanlb1988@gmail.com>
2022-01-13 16:02:22 -0500

    Replace previous stream location change with an if-statement

--- src/my-xxd.cpp
+++ src/my-xxd.cpp
@@ -93,12 +93,15 @@ int write_out_file(vdataptr<std::size_t> &vdata, const OutputName labels, const 
         {
             ss << "\n\t";
         }
-        ss << std::setfill('0') << std::setw(2) << "0x" << std::hex << (0xff & (*vdata)[i]) << ", ";
+        ss << std::setfill('0') << std::setw(2) << "0x" << std::hex << (0xff & (*vdata)[i]);
+        if(i < vdata->size() - 1)
+        {
+            ss << ", ";
+        }
         ofs.write(ss.str().c_str(), ss.str().length());
         ss.str(std::string());
         ss.clear();
     }
-    ofs.seekp(static_cast<std::size_t>(ofs.tellp()) - 5); // Move from position (eof+1) to the position before ", " so we can remove the extra comma in the end
     ss << "\n};\n\nstd::size_t " << labels.name << "_" << labels.ext << "_LEN = " << std::dec << vdata->size() << ";\n\n";
     ss << "#endif /* __" << labels.name << "_" << labels.ext << "_HPP */\n";
     ofs.write(ss.str().c_str(), ss.str().length());
